4e003cefe9e306a80d79fe10860f6d2c9beb5f04
나원진 <77665102+wozzin@users.noreply.github.com>
2021-12-04 01:42:21 +0900

    21.12.04 UI complete

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
+import Card from '../UI/Card';
+
+
+function Expenses(props) {
+
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
