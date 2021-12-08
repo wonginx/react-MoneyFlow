00ac1cd3646de903686ea6c330996e6d79fa3c9f
나원진 <77665102+wozzin@users.noreply.github.com>
2021-12-09 02:15:22 +0900

    Add Expense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -8,7 +9,12 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { 
+      id: "e2", 
+      title: "New TV", 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) 
+    },
     {
       id: "e3",
       title: "Car Insurance",
@@ -26,9 +32,9 @@ function App() {
 
   return (
     <div>
-      <h2>Money Flow</h2>
+      <NewExpense />
       <Expenses items={expenses} />
-        
+      
     </div>
   );
 }
