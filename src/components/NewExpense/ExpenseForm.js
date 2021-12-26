968e70bfe12d79eac66361e9a448fbde4f5d0545
나원진 <77665102+wozzin@users.noreply.github.com>
2021-12-27 02:18:09 +0900

    [Complete] NewExpense -> Cancel button

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -55,10 +55,9 @@ function ExpenseForm(props) {
     };
 
     props.onSaveExpenseData(expenseData);
-    setEnteredTitle('')
-    setEnteredAmount('')
-    setEnteredDate('')
-    
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -94,6 +93,9 @@ function ExpenseForm(props) {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
